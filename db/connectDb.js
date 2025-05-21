21c02cca5fa4d2ce56b1333e23f9f74be2ca3e26
saheelsutar <saheel.y.sutar2004@gmail.com>
2025-05-21 13:39:16 +0530

    Updated code after initial commit

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(`mongodb+srv://stud22sys1:chai@chai.a8g0ykj.mongodb.net/chai`, {
+      const conn = await mongoose.connect(`${process.env.MONGO_URL}`, {
         useNewUrlParser: true,
       });
     } catch (error) {
